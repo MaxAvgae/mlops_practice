4cc7e5c95b78a1fdf0f8ae24f039b59aab67a06f
MaxAvgae <maxavagerus@gmail.com>
2024-01-31 20:16:24 +0300

    Modify 2

--- lab4/modify_2_dataset.py
+++ lab4/modify_2_dataset.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
-
 if __name__ == '__main__':
-
+    # Считываем данные из файла
     train_df = pd.read_csv('titanic.csv')
 
-    train_df = pd.get_dummies(train_df, columns=['Sex'])
+    # Создаем новый столбец 'Sex' на основе 'Sex_female' и 'Sex_male'
+    train_df['Sex'] = train_df['Sex_female'].map({True: 'female', False: 'male'})
+
+    # Удаляем ненужные столбцы 'Sex_female' и 'Sex_male'
+    train_df = train_df.drop(['Sex_female', 'Sex_male'], axis=1)
+
+    # Сохраняем измененные данные
     train_df.to_csv('titanic.csv', index=False)
